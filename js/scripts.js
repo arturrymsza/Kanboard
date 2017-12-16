12076951b1bb3c7cc6ec6b3dadc6fe5664533ddf
Artur Rymsza <artur.rymsza@gmail.com>
2017-12-16 18:17:31 +0100

    Add class-function Column to scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,50 @@
+$(function() {
+	function randomString() {
+		var chars = '0123456789abcdefghiklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXTZ',
+			str = '';
+		for (var i = 0; i < 10; i++) {
+			str += chars[Math.floor(Math.random() * cars.length)];
+		}
+		return str;
+	}
+
+	function Column(name) {
+		var self = this;
+
+		this.id = randomString();
+		this.name = name;
+		this.$element = createColumn();
+
+		function createColumn() {
+			var $column = $('<div>').addClass('column'),
+				$columnTitle = $('<h2>').addClass('column-title').text(self.name),
+				$columnCardList = $('<ul>').addClass('column-card-list'),
+				$columnDelete = $('<button>').addClass('btn-delete').text('x'),
+				$columnAddCard = $('<button>').addClass('add-card').text('Add a card');
+
+			$columnDelete.click(function() {
+				self.removeColumn();
+			});
+
+			$columnAddCard.click(function() {
+				self.addCard(new Card(prompt ("Enter the name of the card")));
+			});
+
+			$column.append($columnTitle)
+					.append($columnDelete)
+					.append($columnAddCard)
+					.append($columnCardList);
+
+			return $column;
+		}
+	}
+
+	Column.prototype = {
+		addCard: function(card) {
+			this.$element.children('ul').append(card.$element);
+		},
+		removeColumn: function() {
+			this.element.remove();
+		}
+	};
+})
