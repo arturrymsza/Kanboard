343bc048b7554d2cd737af1f807f9c5ba5a4d6b6
Artur Rymsza <artur.rymsza@gmail.com>
2017-12-17 13:22:49 +0100

    Add class-function Card to scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -47,4 +47,33 @@ $(function() {
 			this.element.remove();
 		}
 	};
+
+	function Card(description) {
+		var self = this;
+
+		this.id = randomString();
+		this.description = description;
+		this.$element = createCard();
+
+		function createCard(){
+			var $card = $('<li>').addClass('card'),
+				$cardDescription = $('<p>').addClass('card-description').text(self.description),
+				$cardDelete = $('<button>').addClass('btn-delete').text('x');
+
+			$cardDelete.click(function() {
+				self.removeCard();
+			});
+
+			$card.append($cardDelete)
+				.append($cardDescription);
+
+				return $card;
+		}
+	}
+
+	Card.prototype = {
+		removeCard: function() {
+			this.$element.remove();
+		}
+	}
 })
